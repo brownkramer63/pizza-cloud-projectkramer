608fbf3748015de3b15de4c11521489b719fb9d2
Kramer Brown <brownkramer63@gmail.com>
2022-10-03 11:17:20 -0500

    pizzaApplication

--- src/main/java/com/cydeo/controller/DesignPizzaController.java
+++ src/main/java/com/cydeo/controller/DesignPizzaController.java
@@ -5,18 +5,24 @@ import com.cydeo.model.Pizza;
 import com.cydeo.repository.PizzaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
 
 @Controller
-
+@RequestMapping("/design")
 public class DesignPizzaController {
 
     private PizzaRepository pizzaRepository;
 
-    @PostMapping
+
+
+    @GetMapping
     public String showDesignForm(Model model) {
+
+        model.addAttribute("pizza", new Pizza());
 
         model.addAttribute("cheeses", DataGenerator.cheeseTypeList);
         model.addAttribute("sauces", DataGenerator.sauceTypeList);
@@ -35,5 +41,10 @@ public class DesignPizzaController {
         return "redirect:/orders/current?pizzaId=" + pizza.getId();
 
     }
+
+
+
+
+
 
 }
